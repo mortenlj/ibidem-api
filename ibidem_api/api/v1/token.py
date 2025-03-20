594cca374ad7c46ce65a9e8078f437312e69dcdd
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-03-20 15:43:03 +0100

    Get k8s token via tokenrequest API

--- ibidem_api/api/v1/token.py
+++ ibidem_api/api/v1/token.py
@@ -6,6 +6,9 @@ from fastapi import APIRouter, Depends, status
 from joserfc import jwt
 from joserfc.jwk import KeySet
 from joserfc.jwt import JWTClaimsRegistry
+from lightkube import Client
+from lightkube.models.authentication_v1 import TokenRequest, TokenRequestSpec
+from lightkube.models.meta_v1 import ObjectMeta
 from pydantic import BaseModel
 
 LOG = logging.getLogger(__name__)
@@ -45,6 +48,13 @@ CLAIMS = JWTClaimsRegistry(
     },
 )
 
+SUBJECTS = {
+    "mortenlj/javazone": {
+        "namespace": "default",
+        "service_account": "deploy-javazone",
+    }
+}
+
 
 @lru_cache
 def github_keyset():
@@ -53,10 +63,49 @@ def github_keyset():
     return KeySet.import_key_set(resp.json())
 
 
+@lru_cache
+def kube():
+    return Client()
+
+
 @router.post("/", status_code=status.HTTP_200_OK)
-async def token(data: TokenInput, keyset: KeySet = Depends(github_keyset)):
+async def token(
+    data: TokenInput,
+    keyset: KeySet = Depends(github_keyset),
+    kube: Client = Depends(kube),
+):
     """Accept a JWT token and return a new kubernetes token"""
     token = jwt.decode(data.token, key=keyset)
     CLAIMS.validate(token.claims)
     LOG.info("Received valid token for repository: %r", token.claims["repository"])
+    namespace = "default"
+    name = "deploy-javazone"
+    k8s_token = await _get_k8s_token(kube, name, namespace)
+    LOG.info(
+        "Received k8s token for service account %r in namespace %r: %r",
+        name,
+        namespace,
+        k8s_token,
+    )
     return {"token": "valid"}
+
+
+async def _get_k8s_token(kube, name, namespace):
+    token_request = TokenRequest(
+        apiVersion="authentication.k8s.io/v1",
+        kind="TokenRequest",
+        metadata=ObjectMeta(
+            name=name,
+            namespace=namespace,
+        ),
+        spec=TokenRequestSpec(audiences=[]),
+    )
+    req = kube._client._client.build_request(
+        "POST",
+        f"/api/v1/namespaces/{namespace}/serviceaccounts/{name}/token",
+        json=token_request.to_dict(),
+    )
+    resp = kube._client.send(req)
+    resp.raise_for_status()
+    token_request = TokenRequest.from_dict(resp.json())
+    return token_request.status.token
