4248cfdb7826ae6833ee9246e92d5f3c9d5b4858
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-03-20 15:02:30 +0100

    Accept and validate github token

--- ibidem_api/api/v1/token.py
+++ ibidem_api/api/v1/token.py
@@ -1,9 +1,15 @@
 import logging
+from functools import lru_cache
 
-from fastapi import APIRouter, status
+import httpx
+from fastapi import APIRouter, Depends, status
+from joserfc import jwt
+from joserfc.jwk import KeySet
+from joserfc.jwt import JWTClaimsRegistry
 from pydantic import BaseModel
 
 LOG = logging.getLogger(__name__)
+
 
 tags_metadata = [
     {
@@ -17,19 +23,40 @@ router = APIRouter(
 )
 
 
-class Jwt(BaseModel):
+class TokenInput(BaseModel):
     token: str
 
 
-@router.get("/", status_code=status.HTTP_200_OK)
-async def token_get(jwt: Jwt):
-    """Accept a JWT token and return a new kubernetes token"""
-    LOG.info(f"Received token:\n{jwt.token}")
-    return {"token": "get"}
+CLAIMS = JWTClaimsRegistry(
+    iss={
+        "essential": True,
+        "value": "https://token.actions.githubusercontent.com",
+    },
+    aud={
+        "essential": True,
+        "value": "api.ibidem.no:deploy",
+    },
+    repository={
+        "essential": True,
+    },
+    ref={
+        "essential": True,
+        "value": "refs/heads/main",
+    },
+)
+
+
+@lru_cache
+def github_keyset():
+    resp = httpx.get("https://token.actions.githubusercontent.com/.well-known/jwks")
+    resp.raise_for_status()
+    return KeySet.import_key_set(resp.json())
 
 
 @router.post("/", status_code=status.HTTP_200_OK)
-async def token_post(jwt: Jwt):
+async def token(data: TokenInput, keyset: KeySet = Depends(github_keyset)):
     """Accept a JWT token and return a new kubernetes token"""
-    LOG.info(f"Received token:\n{jwt.token}")
-    return {"token": "post"}
+    token = jwt.decode(data.token, key=keyset)
+    CLAIMS.validate(token.claims)
+    LOG.info("Received valid token for repository: %r", token.claims["repository"])
+    return {"token": "valid"}
