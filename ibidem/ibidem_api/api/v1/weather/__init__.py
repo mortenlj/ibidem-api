b05f740e52591fe720275249c361f64bbfde1008
Morten Lied Johansen <mortenjo@ifi.uio.no>
2026-02-21 22:49:16 +0100

    Use async methods for httpx requests

--- ibidem/ibidem_api/api/v1/weather/__init__.py
+++ ibidem/ibidem_api/api/v1/weather/__init__.py
@@ -1,10 +1,11 @@
+import decimal
 import logging
 import tempfile
 from pathlib import Path
 from typing import Annotated
 
 import httpx
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
 
@@ -29,33 +30,35 @@ router = APIRouter(
 
 class WeatherResponse(BaseModel):
     icon_name: str
-    temperature: float
+    temperature: decimal.Decimal
 
 
 async def http_client():
     headers = {"user-agent": f"ibidem-api/{get_version()} +https://github.com/mortenlj/ibidem-api"}
-    with httpx.Client(follow_redirects=True, headers=headers) as client:
+    async with httpx.AsyncClient(follow_redirects=True, headers=headers) as client:
         yield client
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def weather() -> WeatherResponse:
-    return WeatherResponse(icon_name="heavysnowshowersandthunder_polartwilight", temperature=-99.9)
+    return WeatherResponse(icon_name="heavysnowshowersandthunder_polartwilight", temperature=decimal.Decimal("-99.9"))
 
 
 @router.get("/icon/{icon_name}", status_code=status.HTTP_200_OK, response_class=FileResponse)
-async def retrieve_icon(icon_name, http_client: Annotated[httpx.Client, Depends(http_client)]) -> Path:
+async def retrieve_icon(icon_name, http_client: Annotated[httpx.AsyncClient, Depends(http_client)]) -> Path:
     icon_filename = f"{icon_name}.png"
     icon_path = ICON_BASE_DIRECTORY / icon_filename
     if not icon_path.exists():
         icon_path.parent.mkdir(parents=True, exist_ok=True)
         icon_url = f"{ICON_BASE_URL}/{icon_filename}"
-        resp = http_client.get(icon_url)
+        resp = await http_client.get(icon_url)
+        if resp.status_code == status.HTTP_404_NOT_FOUND:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         resp.raise_for_status()
         tmp = tempfile.NamedTemporaryFile("wb", delete=False, dir=icon_path.parent)
         tmp_path = Path(tmp.name)
         with tmp as fobj:
-            for chunk in resp.iter_bytes():
+            async for chunk in resp.aiter_bytes():
                 fobj.write(chunk)
         tmp_path.rename(icon_path)
     return icon_path
